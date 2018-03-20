68366e6167755055cecdae9e0f4d2369cb4ac53b
AGuerrav <aaguerravaldes@gmail.com>
2018-03-19 23:33:57 -0300

    cambio carrousel, haciendo responsive, etc

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,15 +3,7 @@ $(document).ready(function() {
   $('.skills').hide();
   $('#about-me').hide();
   $('.cv').hide();
-  // console.log('holo');
-  // htmlbodyHeightUpdate();
-  // $(window).resize(function() {
-  // 	htmlbodyHeightUpdate();
-  // });;
-  // $(window).scroll(function() {
-  //   height2 = $('.main').height();
-  // 			htmlbodyHeightUpdate();
-  // });;
+
   $('.me').click(function() {
     $('#about-me').show();
     $('.intro').hide();
@@ -48,63 +40,83 @@ $(document).ready(function() {
     $('.skills').hide();
     $('.cv').show();
   });
+  $( ".me" ).on( "click", function() {
+    console.log('holo');
+  $( "#menu-content" ).removeClass( "in");
+});
+$( ".proj" ).on( "click", function() {
+  console.log('holo');
+$( "#menu-content" ).removeClass( "in");
+});
+$( ".skill-menu" ).on( "click", function() {
+  console.log('holo');
+$( "#menu-content" ).removeClass( "in");
+});
+$( ".cv-menu" ).on( "click", function() {
+  console.log('holo');
+$( "#menu-content" ).removeClass( "in");
+});
+$('.icon-info').click(function (event) {
+    $(this).toggleClass('drop');
 });
 
-$(function domReady() {
-  var $image = $('.intro');
-
-  try {
-    $image.ripples({
-      resolution: 512,
-      dropRadius: 20, //px
-      perturbance: 0.04,
-    });
-    $image.ripples('play');
-  } catch (e) {
-    $('.error').show().text(e);
-  }
-
-  // Automatic drops
-  // setInterval(function() {
-  //   var $el = $image;
-  //   var x = Math.random() * $el.outerWidth();
-  //   var y = Math.random() * $el.outerHeight();
-  //   var dropRadius = 25;
-  //   var strength = 0.1 + Math.random() * 0.1;
-  //   $el.ripples('drop', x, y, dropRadius, strength);
-  // }, 400);
 });
-$('.button-about').on('click', function() {
-  $('.intro').ripples('destroy');
-});
-$('.close').on('click', function() {
-  $(function domReady() {
-    var $image = $('.intro');
 
-    try {
-      $image.ripples({
-        resolution: 512,
-        dropRadius: 20, //px
-        perturbance: 0.04,
-      });
-      $image.ripples('play');
-    } catch (e) {
-      $('.error').show().text(e);
-    }
-  });
-});
-function htmlbodyHeightUpdate(){
-var height3 = $( window ).height()
-var height1 = $('.nav').height()+50
-height2 = $('.main').height()
-if(height2 > height3){
-  $('html').height(Math.max(height1,height3,height2)+10);
-  $('body').height(Math.max(height1,height3,height2)+10);
-}
-else
-{
-  $('html').height(Math.max(height1,height3,height2));
-  $('body').height(Math.max(height1,height3,height2));
-}
-
-}
+// $(function domReady() {
+//   var $image = $('.intro');
+//
+//   try {
+//     $image.ripples({
+//       resolution: 512,
+//       dropRadius: 20, // px
+//       perturbance: 0.04,
+//     });
+//     $image.ripples('play');
+//   } catch (e) {
+//     $('.error').show().text(e);
+//   }
+//
+//   // Automatic drops
+//   // setInterval(function() {
+//   //   var $el = $image;
+//   //   var x = Math.random() * $el.outerWidth();
+//   //   var y = Math.random() * $el.outerHeight();
+//   //   var dropRadius = 25;
+//   //   var strength = 0.1 + Math.random() * 0.1;
+//   //   $el.ripples('drop', x, y, dropRadius, strength);
+//   // }, 400);
+// });
+// $('.button-about').on('click', function() {
+//   $('.intro').ripples('destroy');
+// });
+// $('.close').on('click', function() {
+//   $(function domReady() {
+//     var $image = $('.intro');
+//
+//     try {
+//       $image.ripples({
+//         resolution: 512,
+//         dropRadius: 20, //px
+//         perturbance: 0.04,
+//       });
+//       $image.ripples('play');
+//     } catch (e) {
+//       $('.error').show().text(e);
+//     }
+//   });
+// });
+// function htmlbodyHeightUpdate(){
+// var height3 = $( window ).height()
+// var height1 = $('.nav').height()+50
+// height2 = $('.main').height()
+// if(height2 > height3){
+//   $('html').height(Math.max(height1,height3,height2)+10);
+//   $('body').height(Math.max(height1,height3,height2)+10);
+// }
+// else
+// {
+//   $('html').height(Math.max(height1,height3,height2));
+//   $('body').height(Math.max(height1,height3,height2));
+// }
+//
+// }
---
+++ assets/js/slider.js
@@ -0,0 +1,8 @@
+
+$(function () {
+$('.responsiveGallery-wrapper').responsiveGallery({
+animatDuration: 400, //动画时长 单位 ms
+$btn_prev: $('.responsiveGallery-btn_prev'),
+$btn_next: $('.responsiveGallery-btn_next')
+});
+});
