563c795635c4f115840168aba322840aebce4c5c
AGuerrav <aaguerravaldes@gmail.com>
2018-03-26 00:54:20 -0300

    reestructurando mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,14 +6,14 @@ $(document).ready(function() {
 
   $('.me').click(function() {
     $('#about-me').show();
-    $('.intro').hide();
+    $('#home').hide();
     $('.info-proyects').hide();
     $('.skills').hide();
     $('.cv').hide();
   });
   $('.intro-menu').click(function() {
     $('#about-me').hide();
-    $('.intro').show();
+    $('#home').show();
     $('.info-proyects').hide();
     $('.skills').hide();
     $('.cv').hide();
@@ -21,21 +21,21 @@ $(document).ready(function() {
 
   $('.proj').click(function() {
     $('#about-me').hide();
-    $('.intro').hide();
+    $('#home').hide();
     $('.info-proyects').show();
     $('.skills').hide();
     $('.cv').hide();
   });
   $('.skill-menu').click(function() {
     $('#about-me').hide();
-    $('.intro').hide();
+    $('#home').hide();
     $('.info-proyects').hide();
     $('.skills').show();
     $('.cv').hide();
   });
   $('.cv-menu').click(function() {
     $('#about-me').hide();
-    $('.intro').hide();
+    $('#home').hide();
     $('.info-proyects').hide();
     $('.skills').hide();
     $('.cv').show();
@@ -61,62 +61,62 @@ $('.icon-info').click(function (event) {
 });
 
 });
-
-$(function domReady() {
-  var $image = $('.intro');
-
-  try {
-    $image.ripples({
-      resolution: 512,
-      dropRadius: 20, // px
-      perturbance: 0.04,
-    });
-    $image.ripples('play');
-  } catch (e) {
-    $('.error').show().text(e);
-  }
-
-  // Automatic drops
-  // setInterval(function() {
-  //   var $el = $image;
-  //   var x = Math.random() * $el.outerWidth();
-  //   var y = Math.random() * $el.outerHeight();
-  //   var dropRadius = 25;
-  //   var strength = 0.1 + Math.random() * 0.1;
-  //   $el.ripples('drop', x, y, dropRadius, strength);
-  // }, 400);
-});
-$('.button-about').on('click', function() {
-  $('.intro').ripples('destroy');
-});
-$('.close').on('click', function() {
-  $(function domReady() {
-    var $image = $('.intro');
-
-    try {
-      $image.ripples({
-        resolution: 512,
-        dropRadius: 20, //px
-        perturbance: 0.04,
-      });
-      $image.ripples('play');
-    } catch (e) {
-      $('.error').show().text(e);
-    }
-  });
-});
-function htmlbodyHeightUpdate(){
-var height3 = $( window ).height()
-var height1 = $('.nav').height()+50
-height2 = $('.main').height()
-if(height2 > height3){
-  $('html').height(Math.max(height1,height3,height2)+10);
-  $('body').height(Math.max(height1,height3,height2)+10);
-}
-else
-{
-  $('html').height(Math.max(height1,height3,height2));
-  $('body').height(Math.max(height1,height3,height2));
-}
-
-}
+// 
+// $(function domReady() {
+//   var $image = $('#home');
+//
+//   try {
+//     $image.ripples({
+//       resolution: 512,
+//       dropRadius: 20, // px
+//       perturbance: 0.04,
+//     });
+//     $image.ripples('play');
+//   } catch (e) {
+//     $('.error').show().text(e);
+//   }
+//
+//   // Automatic drops
+//   // setInterval(function() {
+//   //   var $el = $image;
+//   //   var x = Math.random() * $el.outerWidth();
+//   //   var y = Math.random() * $el.outerHeight();
+//   //   var dropRadius = 25;
+//   //   var strength = 0.1 + Math.random() * 0.1;
+//   //   $el.ripples('drop', x, y, dropRadius, strength);
+//   // }, 400);
+// });
+// $('.button-about').on('click', function() {
+//   $('#home').ripples('destroy');
+// });
+// $('.close').on('click', function() {
+//   $(function domReady() {
+//     var $image = $('#home');
+//
+//     try {
+//       $image.ripples({
+//         resolution: 512,
+//         dropRadius: 20, //px
+//         perturbance: 0.04,
+//       });
+//       $image.ripples('play');
+//     } catch (e) {
+//       $('.error').show().text(e);
+//     }
+//   });
+// });
+// function htmlbodyHeightUpdate(){
+// var height3 = $( window ).height()
+// var height1 = $('.nav').height()+50
+// height2 = $('.main').height()
+// if(height2 > height3){
+//   $('html').height(Math.max(height1,height3,height2)+10);
+//   $('body').height(Math.max(height1,height3,height2)+10);
+// }
+// else
+// {
+//   $('html').height(Math.max(height1,height3,height2));
+//   $('body').height(Math.max(height1,height3,height2));
+// }
+//
+// }
